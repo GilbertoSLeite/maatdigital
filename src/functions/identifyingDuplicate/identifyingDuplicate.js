3602846e68ccd9ce58b0577641569a4d425eeebd
gilbertoleitels <gleite@lsconsultorias.srv.br>
2021-05-08 22:25:35 -0300

    Atualizando Maat Digital

--- src/functions/identifyingDuplicate/identifyingDuplicate.js
+++ src/functions/identifyingDuplicate/identifyingDuplicate.js
@@ -2,6 +2,5 @@ import SearchDataOrigin from "./searchDataOrigin";
 
 export default async function IdentifyingDuplicate(tabela, index) {
     const originComparation = await SearchDataOrigin(tabela); 
-    console.log('index: ' + index + ' - originComparation: ' + originComparation)
     return (index === originComparation) ? Boolean(true) : Boolean(false)    
 };
